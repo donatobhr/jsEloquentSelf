2a5e87d7460cbd24ae1aa8e2bf1acea06b82eaf4
donato bencosme <donato bencosme>
2017-02-17 00:25:46 -0400

    Ejercicios chap_5 completados

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ var ancestry = JSON.parse(ANCESTRY_FILE);
 
 
 
-// Ejecicio 1
+// Ejercicio 1
 
 // var arrays = [[1,2,3], [4,5], [6]];
 // var array = [];
@@ -89,16 +89,16 @@ var ancestry = JSON.parse(ANCESTRY_FILE);
 
 //Ejercicio 2
 
-function average(array) {
-  function plus(a, b) { return a + b; }
-  return array.reduce(plus) / array.length;
-}
+// function average(array) {
+//   function plus(a, b) { return a + b; }
+//   return array.reduce(plus) / array.length;
+// }
 
-var byName = {};
-ancestry.forEach(function(person) {
-  byName[person.name] = person;
+// var byName = {};
+// ancestry.forEach(function(person) {
+//   byName[person.name] = person;
 
-});
+// });
 
 // var difference = ancestry.filter(function(person){
 // 		return byName[person.mother] != null;
@@ -106,14 +106,72 @@ ancestry.forEach(function(person) {
 // 		return person.born - byName[person.mother].born;
 // 	})
 
-var difference = ancestry.map(function(person){
-	if(byName[person.mother] != null){
-		return person.born - byName[person.mother].born;
-	}else{
-		return null;
-	}
-}).filter(function(person){
-	return person != null;
-})
+// var difference = ancestry.map(function(person){
+// 	if(byName[person.mother] != null){
+// 		return person.born - byName[person.mother].born;
+// 	}else{
+// 		return null;
+// 	}
+// }).filter(function(person){
+// 	return person != null;
+// })
 
-console.log(average(difference));
+// console.log(average(difference));
+
+
+//Ejercicio 3
+
+// function groupBy(array, groupOf){
+// 	var groups = {};
+// 	array.forEach(function(element){
+// 		var groupName = groupOf(element);
+// 		if(groupName in groups){
+// 			groups[groupName].push(element);
+// 		}else{
+// 			groups[groupName] = [element];
+// 		}
+// 	});
+// 	return groups;
+// }
+
+// var byCentury = groupBy(ancestry, function(person){
+// 	return Math.ceil(person.died / 100);
+// })
+
+// for(var century in byCentury){
+// 	var ages = byCentury[century].map(function(person){
+// 		return person.died - person.born;
+// 	})
+// 	console.log(century + ": " + Math.ceil(average(ages)));
+// }
+
+
+// Ejercicio 4
+
+function every(array, predicate){
+	for(var i = 0; i < array.length; i++){
+		if(!predicate(array[i])){
+			return false;
+			continue;
+		}
+	}
+	return true;
+}
+
+function some(array, predicate){
+	for(var i = 0; i < array.length; i++){
+		if(predicate(array[i])){
+			return true;
+			continue;
+		}
+	}
+	return false;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
