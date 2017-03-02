40b4c9e7309a97639a166b6713b2288003e8ac9e
Designer OP <donatobhr@gmail.com>
2017-03-02 12:04:42 -0400

    ejercicio 1 chap_6 completado

--- app.js
+++ app.js
@@ -72,119 +72,176 @@
 // console.log(map.toString());
 
 
-function rowHeights(rows){
-	return rows.map(function(row){
-		return row.reduce(function(max,cell){
-			return Math.max(max, cell.minHeight());
-		}, 0)
-	})
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+// function rowHeights(rows){
+// 	return rows.map(function(row){
+// 		return row.reduce(function(max,cell){
+// 			return Math.max(max, cell.minHeight());
+// 		}, 0)
+// 	})
+// }
+
+// function colWidth(rows){
+// 	return rows[0].map(function(_,i){
+// 		return rows.reduce(function(max, row){
+// 			return Math.max(max, row[i].minWidth());
+// 		},0);
+// 	});
+// }
+
+// function drawTable(rows){
+// 	var heights = rowHeights(rows);
+// 	var widths = colWidth(rows);
+
+// 	function drawLine(blocks, lineNo){
+// 		return blocks.map(function(block){
+// 			return block[lineNo];
+// 		}).join(" ");
+// 	}
+
+
+// 	function drawRow(row, rowNum){
+// 		var blocks = row.map(function(cell, colNum){
+// 			return cell.draw(widths[colNum], heights[rowNum]);
+// 		});
+// 		return blocks[0].map(function(_, lineNo){
+// 			return drawLine(blocks, lineNo);
+// 		}).join("\n");
+// 	}
+
+// 	return rows.map(drawRow).join("\n");
+// }
+
+// function repeat(string, times){
+// 	var result = "";
+// 	for(var i = 0; i < times; i++){
+// 		result += string;
+// 	}
+// 	return result;
+// }
+
+// function TextCell(text){
+// 	this.text = text.split("\n");
+// }
+
+// TextCell.prototype.minWidth = function(){
+// 	return this.text.reduce(function(width, line){
+// 		return Math.max(width, line.length);
+// 	}, 0);
+// };
+
+// TextCell.prototype.minHeight = function(){
+// 	return this.text.length;
+// };
+
+// TextCell.prototype.draw = function(width, height){
+// 	var result = [];
+// 	for(var i = 0; i < height; i++){
+// 		var line = this.text[i] || "";
+// 		result.push(line + repeat(" ", width - line.length));
+// 	}
+// 	return result;
+// };
+
+// var rows = [];
+// for(var i = 0; i < 2; i++){
+// 	var row = [];
+// 	for(var j = 0; j < 2; j++){
+// 		if((j + i) % 2 === 0){
+// 			row.push(new TextCell("##"));
+// 		}else{
+// 			row.push(new TextCell("  "));
+// 		}
+// 	}
+// 	rows.push(row);
+// }
+
+
+// function UnderlinedCell(inner){
+// 	this.inner = inner;
+// };
+
+// UnderlinedCell.prototype.minWidth = function(){
+// 	return this.inner.minWidth();
+// };
+
+// UnderlinedCell.prototype.minHeight = function(){
+// 	return this.inner.minHeight() + 1;
+// }
+
+// UnderlinedCell.prototype.draw = function(width, height){
+// 	return this.inner.draw(width, height - 1).concat([repeat("-", width)]);
+// }
+
+// function dataTable(data){
+// 	var keys = Object.keys(data[0]);
+// 	var headers = keys.map(function(name){
+// 		return new UnderlinedCell(new TextCell(name));
+// 	});
+
+// 	var body = data.map(function(row){
+// 		return keys.map(function(name){
+// 			var value = row[name];
+// 			if(typeof value == "number")
+// 				return new RTextCell(String(row[name]));
+// 			else
+// 				return new TextCell(String(row[name]));
+// 		});
+// 	});
+
+// 	return [headers].concat(body);
+// }
+
+// function RTextCell(text){
+// 	TextCell.call(this,text);
+// }
+
+// RTextCell.prototype = Object.create(TextCell.prototype);
+// RTextCell.prototype.draw = function(width, height){
+// 	var result = [];
+// 	for(var i = 0; i < height; i++){
+// 		var line = this.text[i] || "";
+// 		result.push(repeat(" ", width - line.length) + line);
+// 	}
+
+// 	return result;
+// }
+
+// console.log(drawTable(dataTable(MOUNTAINS)));
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+//Ejercicio 1 Vector Type
+
+function Vector(x,y){
+	this.x = x;
+	this.y = y;
 }
 
-function colWidth(rows){
-	return rows[0].map(function(_,i){
-		return rows.reduce(function(max, row){
-			return Math.max(max, row[i].minWidth());
-		},0);
-	});
+Vector.prototype.plus = function(vector){
+	return new Vector(this.x + vector.x, this.y + vector.y);
 }
 
-function drawTable(rows){
-	var heights = rowHeights(rows);
-	var widths = colWidth(rows);
-
-	function drawLine(blocks, lineNo){
-		return blocks.map(function(block){
-			return block[lineNo];
-		}).join(" ");
-	}
-
-
-	function drawRow(row, rowNum){
-		var blocks = row.map(function(cell, colNum){
-			return cell.draw(widths[colNum], heights[rowNum]);
-		});
-		return blocks[0].map(function(_, lineNo){
-			return drawLine(blocks, lineNo);
-		}).join("\n");
-	}
-
-	return rows.map(drawRow).join("\n");
+Vector.prototype.minus = function(vector){
+	return new Vector(this.x - vector.x, this.y - vector.y);
 }
 
-function repeat(string, times){
-	var result = "";
-	for(var i = 0; i < times; i++){
-		result += string;
-	}
-	return result;
-}
+Object.defineProperty(Vector.prototype, "length",{
+	get: function(){ return Math.sqrt(this.x * this.x + this.y * this.y)}
+});
 
-function TextCell(text){
-	this.text = text.split("\n");
-}
-
-TextCell.prototype.minWidth = function(){
-	return this.text.reduce(function(width, line){
-		return Math.max(width, line.length);
-	}, 0);
-};
-
-TextCell.prototype.minHeight = function(){
-	return this.text.length;
-};
-
-TextCell.prototype.draw = function(width, height){
-	var result = [];
-	for(var i = 0; i < height; i++){
-		var line = this.text[i] || "";
-		result.push(line + repeat(" ", width - line.length));
-	}
-	return result;
-};
-
-var rows = [];
-for(var i = 0; i < 2; i++){
-	var row = [];
-	for(var j = 0; j < 2; j++){
-		if((j + i) % 2 === 0){
-			row.push(new TextCell("##"));
-		}else{
-			row.push(new TextCell("  "));
-		}
-	}
-	rows.push(row);
-}
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+5
 
 
-function UnderlinedCell(inner){
-	this.inner = inner;
-};
-
-UnderlinedCell.prototype.minWidth = function(){
-	return this.inner.minWidth();
-};
-
-UnderlinedCell.prototype.minHeight = function(){
-	return this.inner.minHeight() + 1;
-}
-
-UnderlinedCell.prototype.draw = function(width, height){
-	return this.inner.draw(width, height - 1).concat([repeat("-", width)]);
-}
-
-function dataTable(data){
-	var keys = Object.keys(data[0]);
-	var headers = keys.map(function(name){
-		return new UnderlinedCell(new TextCell(name));
-	});
-
-	var body = data.map(function(row){
-		return keys.map(function(name){
-			return new TextCell(String(row[name]));
-		});
-	});
-
-	return [headers].concat(body);
-}
-
-console.log(drawTable(dataTable(MOUNTAINS)));
+//Ejercicio 2 
