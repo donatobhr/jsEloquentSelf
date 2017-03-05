1258259c92a140a1012cac1b90112586de849a18
donato bencosme <donato bencosme>
2017-03-05 16:05:27 -0400

    chap_6 ejercicio 2,3 completado, chap_6 completado

--- app.js
+++ app.js
@@ -245,3 +245,85 @@
 
 
 //Ejercicio 2 
+
+// function StretchCell(inner, width, height) {
+//   this.inner = inner;
+//   this.width = width;
+//   this.height = height;
+// }
+
+// StretchCell.prototype.minWidth = function() {
+//   return Math.max(this.width, this.inner.minWidth());
+// };
+// StretchCell.prototype.minHeight = function() {
+//   return Math.max(this.height, this.inner.minHeight());
+// };
+// StretchCell.prototype.draw = function(width, height) {
+//   return this.inner.draw(width, height);
+// };
+
+// var sc = new StretchCell(new TextCell("abc"), 1, 2);
+// console.log(sc.minWidth());
+// console.log(sc.minHeight())
+
+
+//Ejercicio 3
+
+function ArraySeq(array){
+	this.pos = -1;
+	this.array = array;
+}
+
+ArraySeq.prototype.next = function(){
+	if(this.pos >= this.array.length - 1){
+		return false;
+	}
+
+	this.pos++;
+	return true;
+}
+
+ArraySeq.prototype.current = function(){
+	return this.array[this.pos];
+}
+
+function RangeSeq(from, to){
+	this.pos = from - 1;
+	this.to = to;
+}
+
+RangeSeq.prototype.next = function(){
+	if(this.pos >= this.to){
+		return false;
+	}
+	this.pos++;
+	return true;
+}
+
+RangeSeq.prototype.current = function(){
+	return this.pos;
+}
+
+// RangeSeq.prototype.length = function(){
+// 	return this.to;
+// }
+// function logFive(sequence){
+// 	for(var i = 0; i < sequence.length(); i++){
+// 		if(!sequence.next()){
+// 			break;
+// 		}
+// 		console.log(sequence.current());
+// 	}
+// }
+
+function logFive(sequence){
+	for(var i = 0; i < 5; i++){
+		if(!sequence.next()){
+			break;
+		}
+		console.log(sequence.current());
+	}
+}
+
+logFive(new ArraySeq([1,2]));
+logFive(new RangeSeq(100,1000));
