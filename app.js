4c3a0c8f468324cdb90111e8422de7942c60674a
donato bencosme <donato bencosme>
2017-04-25 20:17:33 -0400

    chap_8 completado

--- app.js
+++ app.js
@@ -108,3 +108,75 @@ function promptDirection(question){
 
 // InputError.prototype = Object.create(Error.prototype);
 // InputError.prototype.name = "InputError";
+
+
+// ejercicio 1
+
+// function MultiplicatorUnitFailure(message){
+// 	this.message = message;
+// }
+
+// MultiplicatorUnitFailure.prototype = Object.create(Error.prototype);
+// MultiplicatorUnitFailure.prototype.name = "MultiplicatorUnitFailure";
+
+// function primitiveMultiply(a,b){
+// 	if(Math.random() < 0.5){
+// 		return a * b
+// 	}else{
+// 		throw new MultiplicatorUnitFailure();
+// 	}
+// }
+
+// function reliableMultiply(a,b){
+// 	for(;;){
+// 		try{
+// 			return primitiveMultiply(a,b);
+// 		}catch(e){
+// 			if(!(e instanceof MultiplicatorUnitFailure)){
+// 				throw e;
+// 			}
+// 		}		
+// 	}
+// }
+
+// console.log(reliableMultiply(8,8));
+
+
+// ejercicio 2
+var box = {
+	locked: true,
+	unlock: function(){this.locked = false;},
+	lock: function(){this.locked = true;},
+	_content: [],
+	get content(){
+		if(this.locked) throw new Error("Locked!");
+		return this._content
+	}
+};
+
+function withBoxUnlocked(body){
+	var locked = box.locked;
+	if(!locked)
+		return body();
+
+	box.unlock();
+
+	try{
+		return body();
+	}finally{
+		box.lock();
+	}
+}
+
+withBoxUnlocked(function(){
+	box.content.push("Gold Piece");
+})
+
+try{
+	withBoxUnlocked(function(){
+		throw new Error("Pirates on the horizon! Abort!");
+	});
+}catch(e){
+	console.log("Error raised: ", e);
+}
+console.log(box.locked);
