27993e0585f8744a176adec72d9f5c7258a902c8
Donato Bencosme <Donato Bencosme>
2017-05-16 16:05:50 -0300

    continuacion chap_9

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // console.log(/abc/.test("abcde"));
-// console.log(/abc/.test("abxde"));
+// console.log(/abc/.test("abxdec"));
 
 // console.log(/[0123456789]/.test("in 1992"));
 // console.log(/\w/.test("111"));
@@ -47,5 +47,23 @@
 // console.log("Borobudur".replace(/[ou]/g,"a"));
 
 
-console.log("Bencosme, Donato\nLopez, Patricia\nChavez, Dionisio".replace(/([\w ]+), ([\w ]+)/g,"$2 $1"));
+// console.log("Bencosme, Donato\nLopez, Patricia\nChavez, Dionisio".replace(/([\w ]+), ([\w ]+)/g,"$2 $1"));
 
+// var quotedText = /'([^']*)'/;
+// console.log(quotedText.exec("she said 'hello'"));
+
+// console.log(/bad(ly)?/.exec("bad"));
+
+// console.log(/\bcat\b/.test("cat"));
+
+var stock = "1 lemon, 2 cabbages, and 101 eggs";
+function minusOne(match, amount, unit) {
+  amount = Number(amount) - 1;
+  if (amount == 1) // only one left, remove the 's'
+    unit = unit.slice(0, unit.length - 1);
+  else if (amount == 0)
+    amount = "no";
+  return amount + " " + unit;
+}
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+// → no lemon, 1 cabbage, and 100 eggs
