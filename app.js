1fcdbac03f79af8083bcfafdf60197308d240467
donato bencosme <donato bencosme>
2017-05-20 22:46:50 -0400

    primer commit chap_11

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-//chap_10
-
-var month = function() {
-  var names = ["January", "February", "March", "April", "May",
-               "June", "July", "August", "September", "October",
-               "November", "December"];
-  return {
-    name: function(number) { return names[number]; },
-    number: function(name) { return names.indexOf(name); }
-  };
-}();
-
-console.log(month.name(2));
-console.log(month.number("November"));
+//chap_11
