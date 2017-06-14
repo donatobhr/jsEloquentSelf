77acc7ac7958f7ef9517468619c15d0fcf9bc57c
donato bencosme <donato bencosme>
2017-06-13 21:56:05 -0400

    chap_18 completado

--- app.js
+++ app.js
@@ -86,7 +86,7 @@
 
 
 
-var input = document.querySelector("input");
+// var input = document.querySelector("input");
 // input.addEventListener("change", function(){
 // 	if(input.files.length > 0){
 // 		console.log(input.files);
@@ -190,15 +190,123 @@ var input = document.querySelector("input");
 
 
 
+//Ejercicio 1
+// document.querySelector("#button").addEventListener("click",function(){
+// 	var code = document.querySelector("#code").value;
+// 	var outputNode = document.querySelector("#output");
 
-document.querySelector("#button").addEventListener("click",function(){
-	var code = document.querySelector("#code").value;
-	var outputNode = document.querySelector("#output");
+// 	try{
+// 		var result = new Function(code)();
+// 		outputNode.innerText = String(result);
+// 	}catch(e){
+// 		outputNode.innerText = "Error: " + e;
+// 	}
+// });
 
-	try{
-		var result = new Function(code)();
-		outputNode.innerText = String(result);
-	}catch(e){
-		outputNode.innerText = "Error: " + e;
+
+
+//Ejercicio 2
+
+
+// names = ['alert','document','scrollTo'];
+// var terms = [];
+// for(var name in window)
+// 	terms.push(name);
+
+// var textfield = document.querySelector("#field");
+// var suggestions = document.querySelector("#suggestions");
+
+// textfield.addEventListener("input",function(){
+// 	var matching = terms.filter(function(term){
+// 		return term.indexOf(textfield.value) == 0;
+// 	});
+// 	suggestions.textContent = "";
+
+// 	matching.slice(0,20).forEach(function(term){
+// 		var node = document.createElement("div");
+// 		node.textContent = term;
+// 		node.addEventListener("click", function(){
+// 			textfield.value = term;
+// 			suggestions.textContent = "";
+// 		});
+// 		suggestions.appendChild(node);
+// 	});
+// });
+
+
+//Ejercicio 3
+
+var width = 30, height = 15;
+
+var gridNode = document.querySelector("#grid");
+var checkboxes = [];
+
+for(var y = 0; y < height; y++){
+	for(var x = 0; x < width; x++){
+		var box = document.createElement("input");
+		box.type = "checkbox";
+		gridNode.appendChild(box);
+		checkboxes.push(box);
 	}
-});
+	gridNode.appendChild(document.createElement("br"));
+}
+
+function gridFromCheckboxes(){
+	return checkboxes.map(function(box){ return box.checked; });
+}
+
+function checkboxesFromGrid(grid){
+	return grid.forEach(function(value,i){ checkboxes[i].checked = value; })
+}
+
+function randomGrid(){
+	var result = [];
+	for(var i = 0; i < width * height; i++)
+		result.push(Math.random() < 0.3);
+	return result;
+}
+
+checkboxesFromGrid(randomGrid());
+
+function countNeighbors(grid, x, y){
+	var count = 0;
+	for(var y1 = Math.max(0, y - 1); y1 <= Math.min(height - 1, y + 1); y1++){
+		for(var x1 = Math.max(0, x - 1); x1 <= Math.min(width - 1, x + 1); x1++){
+			if((x1 != x || y1 != y) && grid[x1 + y1 * width])
+				count += 1;
+		}
+	}
+	return count;
+}
+
+function nextGeneration(grid){
+	var newGrid = new Array(width * height);
+	for(var y = 0; y < height; y++){
+		for(var x = 0; x < width; x++){
+			var neighbors = countNeighbors(grid, x, y);
+			var offset = x + y * width;
+			if(neighbors < 2 || neighbors > 3)
+				newGrid[offset] = false;
+			else if(neighbors == 2)
+				newGrid[offset] = grid[offset];
+			else
+				newGrid[offset] = true;
+		}
+	}
+	return newGrid;
+}
+
+function turn(){
+	checkboxesFromGrid(nextGeneration(gridFromCheckboxes()));
+}
+
+document.querySelector("#next").addEventListener("click", turn);
+
+var running = null;
+document.querySelector("#run").addEventListener("click",function(){
+	if(running){
+		clearInterval(running);
+	}else{
+		running = setInterval(turn, 400);
+	}
+})
