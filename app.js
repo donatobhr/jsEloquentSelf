f2c861a712209e97d24570bc9b45dd2bac87e36b
donato bencosme <donato bencosme>
2017-06-15 12:02:10 -0400

    chap_19 completado

--- app.js
+++ app.js
@@ -263,4 +263,49 @@ tools["Pick color"] = function(event, cx){
 	cx.strokeStyle = color;
 };
 
+
+function forAllNeighbors(point, fn){
+	fn({x: point.x, y: point.y + 1});
+	fn({x: point.x, y: point.y - 1});
+	fn({x: point.x + 1, y: point.y});
+	fn({x: point.x - 1, y: point.y});
+}
+
+function isSameColor(data,pos1, pos2){
+	var offset1 = (pos1.x + pos1.y * data.width) * 4;
+	var offset2 = (pos2.x + pos2.y * data.width) * 4;
+
+	for(var i = 0; i < 4; i++){
+		if(data.data[offset1 + i] != data.data[offset2 + i])
+			return false;
+	}
+	return true;
+}
+
+
+tools["Flood fill"] = function(event, cx){
+	var startPos = relativePos(event, cx.canvas);
+
+	var data = cx.getImageData(0,0, cx.canvas.width, cx.canvas.height);
+	var alreadyFilled = new Array(data.width * data.height);
+
+	var workList = [startPos];
+	while(workList.length){
+		var pos = workList.pop();
+		var offset = pos.x + data.width * pos.y;
+
+		if(alreadyFilled[offset]) continue;
+
+		cx.fillRect(pos.x, pos.y, 1, 1);
+		alreadyFilled[offset] = true;
+
+		forAllNeighbors(pos,function(neighbor){
+			if(neighbor.x >= 0 && neighbor.x < data.width &&
+			   neighbor.y >= 0 && neighbor.y < data.height &&
+			   isSameColor(data, startPos, neighbor));
+				workList.push(neighbor);
+		});
+	}
+};
+
 createPaint(document.body);
